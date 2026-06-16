0ff976cdac3cde87092cde6701923a0e0a500dae
Erik Ferreira <erik.rocha.ferreira@gmail.com>
2026-06-16 16:36:28 +0800

    New appointmens route page

---
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -0,0 +1,62 @@
+import AppointmentForm from '@/components/forms/AppointmentForm';
+import { getPatientByUserId, getUserById } from '@/lib/actions/patient.actions';
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
+
+type Props = {
+  params: Promise<{
+    userId: string;
+  }>;
+};
+
+async function NewAppointment({ params }: Props) {
+  const { userId } = await params;
+  const [user, patient] = await Promise.all([
+    getUserById(userId),
+    getPatientByUserId(userId),
+  ]);
+
+  if (!user || !patient) {
+    notFound();
+  }
+
+  return (
+    <div className="flex h-screen max-h-screen justify-between">
+      <section className="remove-scrollbar container">
+        <div className="sub-container max-w-6xl">
+          <h1>
+            <Image
+              src="/assets/icons/logo-full.svg"
+              alt="CarePulse Logo"
+              height={1000}
+              width={1000}
+              className="mb-12 h-10 w-fit"
+            />
+          </h1>
+
+          <AppointmentForm
+            userId={user.$id}
+            patientId={patient.$id}
+            primaryPhysician={patient.primaryPhysician}
+          />
+
+          <div className="text-14-regular mt-20 flex justify-between pb-10">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              &copy; {new Date().getFullYear()} CarePulse. All rights reserved.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/appointment-img.png"
+        height={1000}
+        width={1000}
+        alt="Appointment"
+        className="side-img max-w-[40%]"
+      />
+    </div>
+  );
+}
+
+export default NewAppointment;

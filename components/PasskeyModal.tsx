99e6f2636e26cf230962d79c5bcf711849450a52
Erik Ferreira <erik.rocha.ferreira@gmail.com>
2026-06-17 10:42:51 +0800

    added modal functionality

--- components/PasskeyModal.tsx
+++ components/PasskeyModal.tsx
@@ -3,29 +3,71 @@
 import {
   AlertDialog,
   AlertDialogAction,
-  AlertDialogCancel,
   AlertDialogContent,
   AlertDialogDescription,
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
 } from '@/components/ui/alert-dialog';
+
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from '@/components/ui/input-otp';
+import { decryptKey, encryptKey } from '@/lib/utils';
+
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function PasskeyModal() {
   const router = useRouter();
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
+  const [passkey, setPasskey] = useState('');
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const encryptedKey = localStorage.getItem('adminPasskey');
+    const accessKey = encryptedKey && decryptKey(encryptedKey);
+
+    if (accessKey === process.env.NEXT_PUBLIC_ADMIN_PASSKEY) {
+      router.push('/admin');
+      return;
+    }
+
+    const showModal = window.setTimeout(() => {
+      setOpen(true);
+    }, 0);
+
+    return () => {
+      window.clearTimeout(showModal);
+    };
+  }, [router]);
 
   const closeModal = () => {
     setOpen(false);
     router.push('/');
   };
 
+  const validatePasskey = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    if (passkey === process.env.NEXT_PUBLIC_ADMIN_PASSKEY) {
+      setError('');
+      const encryptedkey = encryptKey(passkey);
+
+      localStorage.setItem('adminPasskey', encryptedkey);
+      setOpen(false);
+      router.push('/admin');
+    } else {
+      setError('Invalid passkey. Please try again.');
+    }
+  };
+
   return (
     <AlertDialog open={open} onOpenChange={setOpen}>
-      <AlertDialogContent className="shad-alert-dialog text-dark-700">
+      <AlertDialogContent className="shad-alert-dialog text-dark-700 min-w-125">
         <AlertDialogHeader>
           <AlertDialogTitle className="flex items-start justify-between w-full">
             Admin Access Verification
@@ -39,13 +81,40 @@ function PasskeyModal() {
             />
           </AlertDialogTitle>
           <AlertDialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account from our servers.
+            To access the admin page, please enter the passkey.
           </AlertDialogDescription>
         </AlertDialogHeader>
+
+        <div>
+          <InputOTP
+            maxLength={6}
+            value={passkey}
+            onChange={(value) => setPasskey(value)}
+          >
+            <InputOTPGroup className="shad-otp">
+              <InputOTPSlot className="shad-otp-slot" index={0} />
+              <InputOTPSlot className="shad-otp-slot" index={1} />
+              <InputOTPSlot className="shad-otp-slot" index={2} />
+              <InputOTPSlot className="shad-otp-slot" index={3} />
+              <InputOTPSlot className="shad-otp-slot" index={4} />
+              <InputOTPSlot className="shad-otp-slot" index={5} />
+            </InputOTPGroup>
+          </InputOTP>
+
+          {error && (
+            <p className="shad-error text-14-regular mt-4 flex justify-center">
+              {error}
+            </p>
+          )}
+        </div>
+
         <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Continue</AlertDialogAction>
+          <AlertDialogAction
+            onClick={validatePasskey}
+            className="shad-primary-btn w-full"
+          >
+            Enter Admin Passkey
+          </AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
